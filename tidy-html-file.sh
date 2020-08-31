9542204fbb41df0886467e234da1f154ffdf9606
Michał Wadas <michalwadas@gmail.com>
2020-08-31 12:00:42 +0200

    Update Jekyll and minima versions

--- tidy-html-file.sh
+++ tidy-html-file.sh
@@ -7,5 +7,3 @@ echo "Processing file $(basename "$filename") ($filename)";
 cat "$filename" | tidy -q --new-pre-tags data --custom-tags yes --wrap 240 --indent yes --vertical-space yes --show-info no --show-warnings no > "$temp_file";
 
 cp -f "$temp_file" "$filename";
-
-echo "DONE!"
