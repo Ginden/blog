2aa8c38d563ef5115612050b694f415d1dd2949f
Michał Wadas <michalwadas@gmail.com>
2021-05-30 22:27:12 +0200

    Fix URL in og:image

--- references-workaround/transformators/add-open-graph-data.ts
+++ references-workaround/transformators/add-open-graph-data.ts
@@ -3,6 +3,7 @@ import { JSDOM } from 'jsdom';
 import { copyFile, stat } from 'fs/promises';
 import { join } from 'path';
 import { createDomElement, shortHash } from '../common';
+import {URL} from 'url';
 import slug from 'slug';
 
 const repositoryRoot = join(process.cwd(), '..');
@@ -24,6 +25,7 @@ export async function addOpenGraphData(
     const head = document.head;
 
     if (!head.querySelector('meta[property="og:image"]')) {
+      const { url, baseurl } = document.documentElement.dataset;
         const title = document.querySelector<HTMLMetaElement>(
             'meta[property="og:title"]',
         )!.content;
@@ -52,10 +54,12 @@ export async function addOpenGraphData(
             );
         }
         await copyFile(cacheFilePath, outputPath);
+        const fileUrlFragment = join(baseurl!, 'assets', hashName);
+        const fileUrl = new URL(fileUrlFragment, url!);
         head.appendChild(
             createDomElement(dom, 'meta', {
                 property: 'og:image',
-                content: `/blog/assets/${hashName}`,
+                content: fileUrl.toString(),
             }),
         );
     }
